177d064325eaae963b5c4bdc9c2799c92bcf22db
Skultty <samuel.skultety@outlook.sk>
2023-01-31 13:38:47 +0100

    feat: book status now can be changed and books can be deleted

--- src/main.js
+++ src/main.js
@@ -16,32 +16,60 @@ function addBookToLibrary(author, title, pages, read) {
     myLibrary.push(book);
 }
 
-addBookToLibrary('J. R. R. Tolkien', 'The Hobbit', 295, 'not read yet');
-addBookToLibrary('J. R. R. Tolkien', 'The Lord of the Rings', 1216, 'read');
-addBookToLibrary('J. R. R. Tolkien', 'The Silmarillion', 480, 'not read yet');
-addBookToLibrary('J. R. R. Tolkien', 'The Children of Hurin', 352, 'not read yet');
-addBookToLibrary('J. R. R. Tolkien', 'The Fall of Gondolin', 224, 'not read yet');
+let statusButtons = [];
+let deleteButtons = [];
 
-myLibrary.forEach(book => {
-    // Create a new div element
-    let newDiv = document.createElement('div');
-    // Add a class to the div
-    newDiv.classList.add('book');
-    // Add the div to the content div
-    document.getElementById('content').appendChild(newDiv);
-    // Create a new paragraph element
-    let newP = document.createElement('p');
-    // Add the book info to the paragraph
-    newP.textContent = book.info();
-    // Add the paragraph to the div
-    newDiv.appendChild(newP);
-});
+function render() {
+    document.getElementById('content').innerHTML = '';
+    myLibrary.forEach(book => {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('flex', 'flex-col', 'justify-center', 'items-center', 'border', 'border-black', 'p-4', 'm-4', 'rounded-md', 'bg-gray-200', 'w-1/4');
+        newDiv.innerHTML = `
+        <p>${book.author}</p>
+        <p>${book.title}</p>
+        <p>${book.pages} pages</p>
+        <p>${book.read}</p>
+        <div class="flex  justify-between w-full">
+        <button id="${book.title}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Change status</button>
+        <button id="${book.title} delete" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+        </div>
+        `;
+        document.getElementById('content').appendChild(newDiv);
+        let statusButton = document.getElementById(book.title);
+        document.getElementById(book.title).addEventListener('click', changeStatus);
+        statusButtons.push(statusButton);
+
+        let deleteButton = document.getElementById(book.title);
+        document.getElementById(book.title+" delete").addEventListener('click', deleteBook);
+        deleteButtons.push(deleteButton);
+    });
+}
+
+function changeStatus(event) {
+    let bookTitle = event.target.id;
+    let book = myLibrary.find(book => book.title === bookTitle);
+    if (book.read === 'read') {
+        book.read = 'not read yet';
+    } else {
+        book.read = 'read';
+    }
+    event.target.parentElement.previousElementSibling.textContent = book.read;
+}
+
+function deleteBook(event) {
+    let bookTitle = event.target.id;
+    let book = myLibrary.find(book => book.title === bookTitle);
+    let bookIndex = myLibrary.indexOf(book);
+    myLibrary.splice(bookIndex, 1); 
+    event.target.parentElement.parentElement.remove();
+}
+
 
 const submitButton  = addEventListener('submit', submitForm);
 
 function submitForm (event) {
     event.preventDefault();
-    let author = document.getElementById('author').value;
+    let author = document.getElementById('name').value;
     let title = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').checked;
@@ -55,12 +83,7 @@ function submitForm (event) {
     }
 
     addBookToLibrary(author, title, pages, read);
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('book');
-    document.getElementById('content').appendChild(newDiv);
-    let newP = document.createElement('p');
-    newP.textContent = `${title} by ${author}, ${pages} pages, ${read}`;
-    newDiv.appendChild(newP);
+    render();
 
     document.getElementById('closeModal').click();
     
